22a92e523f20e6e9229c1cb12aa1a3d635af3f74
Anisa <anisa.ali7@gmail.com>
2017-07-17 15:40:52 -0500

    select key class on keydown

--- client.js
+++ client.js
@@ -6,6 +6,12 @@ window.addEventListener('keydown', function(e) {
 //return a list of selected audio elements within the document
 //ES6 template strings/literals: `${}` - placeholder for string substitution
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+
   if (!audio) return; //stops function from running
+  //rewind audio to the start
+  audio.currentTime = 0;
   audio.play();
+
+  console.log(key);
 });
