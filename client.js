f7bfd2cf64a6634ff494ecf1d296372c3d2ecd29
Anisa <anisa.ali7@gmail.com>
2017-07-17 14:55:18 -0500

    stop func if no audio for key

--- client.js
+++ client.js
@@ -3,8 +3,11 @@ console.log('JS Sourced!');
 //activated when key(A - L) is pressed down
 window.addEventListener('keydown', function(e) {
 
-// return a list of selected audio elements within the document
+//return a list of selected audio elements within the document
+//ES6 template strings/literals: `${}` - placeholder for string substitution
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  if (!audio) return; //stops function from running
+
   //check if selected an actual audio element
   console.log(audio);
 });
