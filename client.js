e1b171e160334439dfd44855f544d60c7da46836
Anisa <anisa.ali7@gmail.com>
2017-07-17 15:59:13 -0500

    remove playing class after transition

--- client.js
+++ client.js
@@ -8,11 +8,18 @@ window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
   const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
 
-  if (!audio) return; //stops function from running
-  //rewind audio to the start
-  audio.currentTime = 0;
+  if (!audio) return;     //stops function from running
+  audio.currentTime = 0;   //rewind audio to the start
   audio.play();
 
-  key.classList.add('playing');
-
+  key.classList.add('playing');   //add class 'playing' w/ CSS effect
 });
+
+function removeTransition(e){
+  if (e.propertyName !== 'transform') return; // skip if not transform
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+//add transition end to each key, then run removeTransition()
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
