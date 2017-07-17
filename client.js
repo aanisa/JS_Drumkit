9eebc05b4fec25e578ace636eba306760a46f404
Anisa <anisa.ali7@gmail.com>
2017-07-17 14:49:22 -0500

    return audio for selected key

--- client.js
+++ client.js
@@ -2,5 +2,9 @@ console.log('JS Sourced!');
 
 //activated when key(A - L) is pressed down
 window.addEventListener('keydown', function(e) {
-  console.log(e);
+
+// return a list of selected audio elements within the document
+  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  //check if selected an actual audio element
+  console.log(audio);
 });
