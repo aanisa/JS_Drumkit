946141d1efead5d04ad674087a10fb0b337e4907
Anisa <anisa.ali7@gmail.com>
2017-07-17 16:02:07 -0500

    playAudio function, call when keydown

--- client.js
+++ client.js
@@ -1,19 +1,21 @@
 console.log('JS Sourced!');
 
 //activated when key(A - L) is pressed down
-window.addEventListener('keydown', function(e) {
+window.addEventListener('keydown', playAudio);
 
-//return a list of selected audio elements within the document
-//ES6 template strings/literals: `${}` - placeholder for string substitution
-  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-  const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+function playAudio(e){
 
-  if (!audio) return;     //stops function from running
-  audio.currentTime = 0;   //rewind audio to the start
-  audio.play();
+  //return a list of selected audio elements within the document
+  //ES6 template strings/literals: `${}` - placeholder for string substitution
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
 
-  key.classList.add('playing');   //add class 'playing' w/ CSS effect
-});
+    if (!audio) return;     //stops function from running
+    audio.currentTime = 0;   //rewind audio to the start
+    audio.play();
+
+    key.classList.add('playing');   //add class 'playing' w/ CSS effect
+}
 
 function removeTransition(e){
   if (e.propertyName !== 'transform') return; // skip if not transform
